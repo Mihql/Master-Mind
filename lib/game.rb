ceeb980ac34067466d85bd76ee001d0295a78b79
Mihql <vipercoc36@gmail.com>
2024-10-28 19:18:50 +0530

    Add user feedback
    - Added user feedback of the color choose based on v/s opponent color code

--- lib/game.rb
+++ lib/game.rb
@@ -13,10 +13,15 @@ class Game
 
     @computer_color_code = generate_random_color(COUNT)
     @guessed_color_code = Array.new(COUNT) { Array.new(COUNT) }
-    @computer_guessed_code = @computer_color_code.sample[4]
+    @computer_guessed_code = @computer_color_code.keys.sample(4, random: Random.new(1))
+    @computer_guessed_code.each do |key|
+      @code = key
+    end
+    puts "#{@computer_guessed_code}  - COMPUTER CODE"
     @computer = 'O'
     @player = 'X'
     @level = 0 # Initial -1 because increment array
+    @player_guessed_right = 0
   end
 
   def generate_random_color(count)
@@ -37,19 +42,31 @@ class Game
     puts @computer_color_code.inspect
     COUNT.times do
       @board.display_board
-      color = prompt_color
-      puts "Color: #{color.inspect}"
-
-      if get_guess_color(color).last
-        puts "#{color} Found"
-        puts "Guessed color code placed: #{@guessed_color_code}"
-      else
-        puts 'Foo'
-      end
+      # result_boolean = level()
+      prompt_color
+      # puts "Color: #{color.inspect}"
     end
 
     @board.display_board
     # puts "Guessed color codes are: #{@guessed_color_code}"
+  end
+
+  def level(user_input)
+    result = get_computer_guess_color(user_input)
+    puts "Lets see the #{result.last}"
+    if result.last
+      color = result[0]
+      puts "#{color} You Found It !!!"
+      @player_guessed_right += 1
+    else
+      puts "DUMB!!! #{user_input} is not here!"
+    end
+
+    feedback(user_input, @player_guessed_right)
+  end
+    
+  def feedback(color, player_guessed_right)
+    puts "You guessed #{color} of #{player_guessed_right} / 5"
   end
 
   def prompt_color
@@ -59,10 +76,15 @@ class Game
         user_input = gets.chomp
 
         if user_input.is_a?(String) && valid_color_name?(user_input)
-          colored = Color::RGB.by_name(user_input).html.upcase
-          @guessed_color_code[@level][i] = colored
+          colored = Color::RGB.by_name(user_input).html.upcase # covert to hex
+          # @guessed_color_code[@level][i] = colored        #// hex 
+          @guessed_color_code[@level][i] = user_input   # "color_name" 
           add_guess(user_input, @level, i)
           @board.display_board
+
+          level(user_input)
+          puts "ITS BREAK HERE NO WONDERS!"
+          puts "#{@computer_guessed_code}  - COMPUTER CODE"
           break # Break out the inner loop
         else  
           puts "invalid color input #{user_input}"
@@ -72,7 +94,9 @@ class Game
     end
 
     @level += 1
+    puts "DOES IT BREAK?"
     @guessed_color_code
+    level(@guessed_color_code)
   end
 
   def valid_color_name?(color_name)
@@ -87,6 +111,17 @@ class Game
     @computer_color_code.each do |color_name, color_hex|
       if color_hex == color
         puts "Color-Hex: #{color_hex} | Color-name: #{color_name} | Color: #{color}"
+        return [color, true]
+      end
+    end
+
+    [color, false]
+  end
+
+  def get_computer_guess_color(color)
+    @computer_guessed_code.each do |color_key|
+      if color_key == color
+        puts "#{color_key} == #{color}"
         return [color, true]
       end
     end
